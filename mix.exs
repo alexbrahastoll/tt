f8d7e67107af913e343561527ac310f07c3113a3
Alex Braha Stoll <alexbrahastoll@gmail.com>
2019-06-16 15:16:13 -0300

    Changes escript output path

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TT.MixProject do
       app: :tt,
       version: "0.1.0",
       elixir: "~> 1.8",
-      escript: [main_module: TT.CLI],
+      escript: [main_module: TT.CLI, path: "escript/tt"],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
